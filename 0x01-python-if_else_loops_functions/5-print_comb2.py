0d8a5082ed4a710a756224be8985b65363155d93
mittsahl <msalem311@aol.com>
2020-12-08 18:42:49 +0000

    Fixed formatting

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 
-for num in range(0, 100):
-    if num == 99:
-        print("{}".format(num))
-    elif num < 10:
-        print("{}{}".format(num % 1, num), end=", ")
-    else:
-        print("{}".format(num), end=", ")
+for num in range(0, 99):
+    print("{:02d}, ".format(num), end='')
+print(99)
