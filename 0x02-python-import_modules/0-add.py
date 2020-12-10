b75e109c123e85d0a982976006a494c544efb0f9
mittsahl <msalem311@aol.com>
2020-12-10 18:53:56 +0000

    Added i fname is main

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
-import add_0
+from add_0 import add
 
-a = 1
-b = 2
-print("{} + {} = {}".format(str(a), str(b), str(add_0.add(a, b))))
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(str(a), str(b), str(add(a, b))))
 
