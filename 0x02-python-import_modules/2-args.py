fc6d492a90e2e52e371920d2301af22ff7ad18bc
mittsahl <msalem311@aol.com>
2020-12-10 19:28:43 +0000

    Prints the number of arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+from sys import argv
+
+if __name__ == "__main__":
+    if (len(argv) == 1):
+        print("0 arguments.")
+    elif (len(argv) == 2):
+        print("1 argument:\n1: {}".format(argv[1]))
+    else:
+        print("{} arguments:".format(len(argv) - 1))
+        for arg in range(1, len(argv)):
+            print("{}: {}".format(arg, argv[arg]))
