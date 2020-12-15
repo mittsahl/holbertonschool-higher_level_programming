da1f77f54dd916564dd7aef529ea3d8b61cee58f
mittsahl <msalem311@aol.com>
2020-12-15 20:41:59 +0000

    Added check if idx of my_list is None

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    if idx < 0 or idx > len(my_list) or idx == None or my_list == None:
+    if idx < 0 or idx > len(my_list) or idx is None or my_list is None:
         return None
     return my_list[idx]
