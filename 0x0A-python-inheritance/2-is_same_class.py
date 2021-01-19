f9a310255c084c8c2740b89cdbff4e4192f889ca
mittsahl <msalem311@aol.com>
2021-01-19 21:00:06 +0000

    Fixed formatting

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""Function that checks if they data types are the same"""
+"""Function that checks"""
 
 
 def is_same_class(obj, a_class):
-    """Function that checks if they are the same data type"""
     return type(obj) is a_class
