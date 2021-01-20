678656f4bd3fe0e8d23a933f34ba5a99d61c11f7
mittsahl <msalem311@aol.com>
2021-01-20 19:17:56 +0000

    Reads a file and writes to stdout

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This program reads a file and prints it to standard out"""
+
+
+def read_file(filename=""):
+    """Reads file and prints"""
+    with open("my_file_0.txt") as f:
+        print(f.read())
