b51016deb151e7c3401a197d560980ba57de3b09
mittsahl <msalem311@aol.com>
2021-01-20 04:40:27 +0000

    Fixed formatting

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -2,8 +2,8 @@
 """Function that checks to see if the instance of a class"""
 
 
-"""Function that checks"""
 def inherits_from(obj, a_class):
+    """returns false if the clases and object are the same"""
     if type(obj) is a_class:
         return False
     return (issubclass(type(obj), a_class))
