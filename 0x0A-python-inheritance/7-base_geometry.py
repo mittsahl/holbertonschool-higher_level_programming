e6a9cf2edb2667a2e3c4df313344ec4b0d0c2893
mittsahl <msalem311@aol.com>
2021-01-21 05:05:01 +0000

    Changed formatting of comments

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -8,9 +8,9 @@ class BaseGeometry:
         """Throws an error when not made"""
         raise Exception("area() is not implemented")
 
-    def integer_validator(self, name="", value=0):
+    def integer_validator(self, name, value):
         """Checks the data type of value"""
         if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
+            raise TypeError("{:s} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+            raise ValueError("{:s must be greater than 0".format(name))
