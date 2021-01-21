789add69b1d20c0776e0b36f5d08fa9d4f6411eb
mittsahl <msalem311@aol.com>
2021-01-21 15:29:31 +0000

    Defines class square that inherits from rectangle

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This file defines saure that inherits from rectangle"""
+
+
+Rectangle = __import__('9-rectnagle').Rectangle
+
+
+class Square(Rectangle):
+    """Class definition of square"""
+
+    def __init__(self, size):
+        """Initialization funcitons"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
