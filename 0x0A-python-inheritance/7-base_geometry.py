d472e32ac624ce30daf4029424382922dca130c1
mittsahl <msalem311@aol.com>
2021-01-21 14:58:25 +0000

    Fixed formatting

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -4,6 +4,7 @@
 
 class BaseGeometry:
     """Class definition"""
+
     def area(self):
         """Throws an error when not made"""
         raise Exception("area() is not implemented")
@@ -12,5 +13,5 @@ class BaseGeometry:
         """Checks the data type of value"""
         if type(value) is not int:
             raise TypeError("{:s} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{:s must be greater than 0".format(name))
+        elif value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
