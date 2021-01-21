21245b10c483358c210ec0445a2e75d91f50f103
mittsahl <msalem311@aol.com>
2021-01-21 03:36:18 +0000

    Fixed formatting

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""File converts JS object to a string"""
+"""File converts object to a JSON representation"""
 import json
 
 
 def to_json_string(my_obj):
-    return(json.dumps(my_obj))
+    """returns the object, json format"""
+    return json.dumps(my_obj)
