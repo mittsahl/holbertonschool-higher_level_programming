9ca0eb75124b0af25922af86672ce48123f7b5c6
mittsahl <msalem311@aol.com>
2021-01-21 05:48:16 +0000

    Inherits from task 7

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python3
 """Creating another class with inheritance"""
+geo = __import__('7-base_geometry').BaseGeometry
 
-
-geo = __import__('8-rectangle').Rectangle
 
 class Rectangle(geo):
-    
+    """Class definition"""
+    def __init__(self, width, height):
+        """Initilaization function"""
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """Finds the area of the rectangle"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Finds the dimensions of the rectangle"""
+        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
