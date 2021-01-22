99152148c3cebe26e6a274d5b33bfb8587fe73cf
mittsahl <msalem311@aol.com>
2021-01-22 04:29:12 +0000

    Fixed typo in function name

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -4,12 +4,13 @@
 
 class Student:
     """Defines class student"""
+
     def __init__(self, first_name, last_name, age):
         """Initialization method"""
-        self.first name = first_name
+        self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_josn(self):
+    def to_json(self):
         """Converts to a dict"""
         return self.__dict__
