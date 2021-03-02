dd0f843607d16e980e3ec4af3fa55f652b9aa49d
mittsahl <msalem311@aol.com>
2021-03-02 19:52:08 +0000

    Creates table with ofreign key that references a states table

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -5,6 +5,7 @@ USE htbn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
 	id INT NOT NULL AUTO_INCREMENT,
 	state_id INT NOT NULL,
-	FOREIGN KEY (state_id) REFERENCES state(id),
-	name VARCHAR(256) NOT NULL
+	name VARCHAR(256) NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (state_id) REFERENCES states(id)
 	);
