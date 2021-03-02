c3965bce3d7b6b909f45344f220fc5bafd7bd4bf
mittsahl <msalem311@aol.com>
2021-03-02 18:58:44 +0000

    Added grant usage statement

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,4 +2,5 @@
 -- read privileges and create database
 CREATE DATABASE IF NOT EXISTS  htbn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT USAGE ON *.* TO 'user_0d_2'@'localhost';
 GRANT SELECT ON htbn_0d_2.* TO 'user_0d_2'@'localhost';
