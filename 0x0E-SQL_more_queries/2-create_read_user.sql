fbac6163a60ed6ced67363a2e813c0bb091287e5
mittsahl <msalem311@aol.com>
2021-03-02 19:09:39 +0000

    Changed how setting the passwoed

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,6 @@
 -- Creates User with only
 -- read privileges and create database
 CREATE DATABASE IF NOT EXISTS  htbn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT USAGE ON *.* TO 'user_0d_2'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+SET PASSWORD FOR 'user_0d_2'@'localhost' = 'user_0d_2_pwd';
 GRANT SELECT ON htbn_0d_2.* TO 'user_0d_2'@'localhost';
