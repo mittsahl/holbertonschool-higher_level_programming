99f33c6ee46dfdd2e251f7762a99607ec162cb39
mittsahl <msalem311@aol.com>
2021-03-02 16:29:38 +0000

    Creates user and grants all privileges

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- Creates user and sets password
+-- doesnt fail if it exists
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
