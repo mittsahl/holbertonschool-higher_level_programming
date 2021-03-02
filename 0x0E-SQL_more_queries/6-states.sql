40a9bb8adc7a90100aa4619c7a5b369031b8ef2b
mittsahl <msalem311@aol.com>
2021-03-02 19:36:25 +0000

    Creates database and table with auto generated id

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,9 @@
+-- Creates database htbn_0d_usa 
+-- and table states
+CREATE DATABASE IF NOT EXISTS htbn_0d_usa;
+USE htbn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+	id INT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(256) NOT NULL,
+	PRIMARY KEY (id)
+	);
