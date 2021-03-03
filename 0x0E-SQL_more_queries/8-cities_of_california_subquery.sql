fef24297d0570183e0a07586dd48e90c70f3f2b8
mittsahl <msalem311@aol.com>
2021-03-03 18:18:13 +0000

    Changed formatting of subquery

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,12 +1,7 @@
 -- Lists all cities of california
 -- db passed as arg
-SELECT id, name
-FROM
-	cities
-WHERE
-	state_id IN (SELECT
-		id
-	FROM
-		states
-	WHERE
-		id = state_id);
+SELECT id, name FROM cities
+WHERE state_id = 
+(SELECT id FROM states
+WHERE name = 'California')
+ORDER BY cities.id ASC;
